2da53cd9d518d0926ad59741afd5b19ed3e3ba46
Sunil Hari <isunilhari@gmail.com>
2018-04-26 18:40:03 +0000

    Add option to format the date output (#3)

--- cli.js
+++ cli.js
@@ -1,3 +1,38 @@
 #!/usr/bin/env node
 'use strict';
-console.log(Date.now());
+const meow = require('meow');
+const formatter = require('date-fns/format');
+const terminalLink = require('terminal-link');
+
+const termLink = terminalLink('date-fns', 'https://date-fns.org/v1.29.0/docs/format');
+const cli = meow(`
+      Usage
+        $ date-now
+      Options
+        --format <format>, -f Get Formatted Output
+      Examples
+        $ date-now
+          1524733860619 //Output
+        $ date-now --format  "DD.MM.YYYY"
+          26.04.2018 //Output
+        For supported date formats please visit ${termLink}
+    `, {
+	flags: {
+		format: {
+			type: 'string',
+			alias: 'f'
+		}
+	}
+}
+);
+if ('format' in cli.flags) {
+	const {format} = cli.flags;
+	if (format) {
+    console.log(formatter(new Date(), format));
+	} else {
+    console.log(`Please specify a format.Visit  ${termLink} for supported date formats`);
+	}
+} else {
+  console.log(Date.now());
+}
+
--- test.js
+++ test.js
@@ -5,3 +5,9 @@ test('main', async t => {
 	const {stdout} = await execa('./cli.js');
 	t.regex(stdout, /\d+/);
 });
+
+test('Get Formatted Text', async t => {
+	const {stdout} = await execa('./cli.js', ['--format', 'YYYY']);
+	const expectedOut = new Date().getFullYear();
+	t.is(Number(stdout), expectedOut);
+});
